e22149cc790444c6a00fcc074964052ad8d4254c
ScaryBull <yaitskaya13varvar@gmail.com>
2026-01-16 01:11:37 +0700

    .

---
+++ lab4/flat_hash_map.h
@@ -0,0 +1,8 @@
+//
+// Created by yaits on 15.01.2026.
+//
+
+#ifndef LAB4_FLAT_HASH_MAP_H
+#define LAB4_FLAT_HASH_MAP_H
+
+#endif //LAB4_FLAT_HASH_MAP_H
---
+++ lab4/test_flat_hash_map.cpp
@@ -0,0 +1,3 @@
+//
+// Created by yaits on 15.01.2026.
+//
